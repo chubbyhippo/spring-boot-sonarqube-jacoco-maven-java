d73fe7ede0105fee5cb946acd6b5e48bdb849043
ch <mkmsacc@outlook.com>
2023-12-30 20:18:30 +0700

    Add HelloController and update tests
    Added a new HelloController with a GET mapping to "/hello". Updated DemoApplicationTests to include a new test for this endpoint, utilizing WebTestClient to send a get request and asserting the response.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,17 +1,34 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
+    @Autowired
+    private WebTestClient webTestClient;
+
     @Test
     @DisplayName("should not throw any errors")
     void shouldNotThrowAnyErrors() {
         assertThatNoException().isThrownBy(() -> DemoApplication.main(new String[]{"--server.port=0"}));
+    }
+
+    @Test
+    @DisplayName("should return hello")
+    void shouldReturnHello() {
+        webTestClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/hello").build())
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo("hello");
+
     }
 }
