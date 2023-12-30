27de0a4655e61561605312b8d0d0a3ff877da3da
ch <mkmsacc@outlook.com>
2023-12-30 20:15:06 +0700

    Update Spring Boot test configuration and assertions
    Updated Spring Boot test configuration to use random server port in tests. Also, implemented a new test method to check that the main application is not throwing any exceptions. The test method employs AssertJ's assertThatNoException() for verification.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    @DisplayName("should not throw any errors")
+    void shouldNotThrowAnyErrors() {
+        assertThatNoException().isThrownBy(() -> DemoApplication.main(new String[]{"--server.port=0"}));
+    }
 }
